cf721264f93e3e6e7c5de22da96d82ba35f7fd74
Junichi Ito <jit@sonicgarden.jp>
2017-07-09 13:49:00 +0900

    Tweak UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
+  # http://qiita.com/walakka-jp/items/339595ab43439bab2518
   def bootstrap_class_for flash_type
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
+  # http://qiita.com/walakka-jp/items/339595ab43439bab2518
   def flash_messages(opts = {})
     flash.each do |flash_type, message|
       concat(
