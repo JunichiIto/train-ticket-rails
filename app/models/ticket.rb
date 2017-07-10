2b4ef65dc6ffc9881080849cfdf948d1125fe19b
Junichi Ito <jit@sonicgarden.jp>
2017-07-11 08:56:53 +0900

    Remove unused scope

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,4 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
-
-  scope :not_exited, -> { where(exited_gate_id: nil) }
 end
