256ce189f168954da13b6567e12420c879c3c355
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-07-12 08:05:30 +0900

    complete exercise

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -1,6 +1,6 @@
 # NOTE: Gate = 改札機のイメージ
 class Gate < ApplicationRecord
-  FARES = [150, 190]
+  FARES = [150, 190].freeze
 
   validates :name, presence: true, uniqueness: true
   validates :station_number, presence: true, uniqueness: true
@@ -8,6 +8,15 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    return false if ticket.entered_gate == self
+    fare_from(ticket.entered_gate) <= ticket.fare
+  end
+
+  def distance_from(departure)
+    (self.station_number - departure.station_number).abs - 1
+  end
+
+  def fare_from(destination)
+    FARES[distance_from(destination)]
   end
 end
