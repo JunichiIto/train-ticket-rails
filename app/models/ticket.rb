27342d8c741c38b5fa3a00860aff71eb30bc3c7e
vividmuimui <tkoyama@aiming-inc.com>
2017-07-16 22:16:43 +0900

    Answer Ex4

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,14 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+  validate :ensure_exitable_gate
+
+  private
+
+  def ensure_exitable_gate
+    return unless exited_gate_id
+    unless exited_gate.exit?(self)
+      errors.add(:exited_gate_id, :not_exitable_gate)
+    end
+  end
 end
