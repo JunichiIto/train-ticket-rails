fb737d5aa845f195fee75b6d49db64368d4e5f7d
yuemori <yuemori@aiming-inc.com>
2017-07-16 14:52:23 +0900

    Implement Gate#exit?

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,13 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    section_number = (ticket.entered_gate.station_number - station_number).abs
+
+    case section_number
+    when 1 then ticket.fare == 150
+    when 2 then ticket.fare == 190
+    else
+      false
+    end
   end
 end
