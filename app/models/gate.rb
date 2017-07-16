119d62438e2dc1cfa14a75bcb0173a54441824cf
284km <at284km@gmail.com>
2017-07-16 21:26:40 +0900

    Implements Ex1

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,12 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    ticket.fare >= section_fare(ticket.entered_gate)
+  end
+
+  private
+
+  def section_fare(entered_gate)
+    FARES[(station_number - entered_gate.station_number).abs - 1]
   end
 end
