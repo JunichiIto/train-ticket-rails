286143e859b86a078cf38ee30ea110dfd76fbc53
Junichi Ito <jit@sonicgarden.jp>
2017-07-17 16:07:14 +0900

    Revert answer

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,14 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+  validate :validate_exited_gate, if: -> { exited_gate.present? }
+  def validate_exited_gate
+    unless exited_gate.exit?(self)
+      errors.add(:exited_gate, 'では降車できません。')
+    end
+  end
+
+  def exited?
+    exited_gate.present?
+  end
 end
