d9f528c23903d19b917c40d27ca45700879eae9b
cohakim <cohakim@gmail.com>
2017-07-24 02:28:26 +0900

    Ex4. 画面の実装 を実装

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,12 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+
+  validate :can_get_off?, on: :update
+
+  def can_get_off?
+    unless exited_gate.exit?(self)
+      errors[:exited_gate] << 'では降車できません。'
+    end
+  end
 end
