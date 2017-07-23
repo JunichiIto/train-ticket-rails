5774743e51facd86a992e67c68bd1992d4f2ecd1
cohakim <cohakim@gmail.com>
2017-07-24 01:34:41 +0900

    Ex1. 運賃の計算（下り）を実装

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,18 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    ticket.fare >= calculate_fare(ticket)
+  end
+
+  def calculate_fare(ticket)
+    case calculate_boarding_section(ticket)
+    when 1; Gate::FARES[0]
+    when 2; Gate::FARES[1]
+    else raise 'fare is not defined'
+    end
+  end
+
+  def calculate_boarding_section(ticket)
+    (station_number - ticket.entered_gate.station_number).abs
   end
 end
