bf1ac06e571b5ced27924a342d13ca43eb2da1b0
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-07-29 11:49:21 +0900

    Ex1. 運賃の計算（下り）を実装

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,8 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    entered_gate = Gate.find(ticket.entered_gate_id)
+    section = (self.station_number - entered_gate.station_number).abs
+    ticket.fare >= FARES[section - 1]
   end
 end
