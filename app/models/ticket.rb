fc37eb4232658bd50b4494a8f689d0df304d988b
sinsoku <sinsoku.listy@gmail.com>
2017-08-01 01:03:13 +0900

    :sparkles: Fix a test for Ex4

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,12 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+
+  validate :must_be_exit, if: :exited_gate_id
+
+  private
+
+  def must_be_exit
+    errors.add(:exited_gate, 'では降車できません。') unless exited_gate.exit?(self)
+  end
 end
