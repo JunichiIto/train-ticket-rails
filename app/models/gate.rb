8ae8f8738643f7b8d72bb8ad0864b3997af2a8f2
sinsoku <sinsoku.listy@gmail.com>
2017-08-01 00:36:40 +0900

    :sparkles: Fix a test for Ex1

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,8 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    section = (station_number - ticket.entered_gate.station_number).abs
+    price = FARES[section - 1]
+    ticket.fare - price >= 0
   end
 end
