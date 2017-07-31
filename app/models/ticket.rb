176456a100a8008ff3ebac3cfcf0ced3f19a2f93
sinsoku <sinsoku.listy@gmail.com>
2017-08-01 01:31:26 +0900

    :recycle: Use optional instead of required
    Because the rquired option is deprecated from Rails v5.0.
    
    see also: http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#method-i-belongs_to

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :entered_gate, class_name: 'Gate', foreign_key: 'entered_gate_id'
-  belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
+  belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', optional: true
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
 
