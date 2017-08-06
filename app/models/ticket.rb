aa142fbdf3496b464614ac63fa2b26551649b903
itume <jem90ham@gmail.com>
2017-08-06 11:04:15 +0900

    降車できない場合のエラーが正しく表示されるように

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,9 +3,14 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+  validate :can_exit?, on: :update
 
   RIDE_SECTION_FARES = {1 => 150, 2 => 190}
   private_constant :RIDE_SECTION_FARES
+
+  def can_exit?
+    @errors.add(:exited_gate_id,"では降車できません。") unless exited_gate.exit?(self)
+  end
 
   def pay_enough?(ride_section)
     return false if ride_section == 0
