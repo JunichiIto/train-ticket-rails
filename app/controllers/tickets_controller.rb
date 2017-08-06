e491fec147e1b5c4ae74071828d5d273abb0d60a
yusuke.saito <yusuke.saito@smartscape.co.jp>
2017-08-06 17:24:15 +0900

    お題に回答

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,9 @@
 class TicketsController < ApplicationController
   before_action :load_ticket, only: %i(edit update show)
 
+  TICKET_GOT_OFF = "降車済みの切符です。".freeze
+  CAN_NOT_GET_OFF = "降車駅 では降車できません。".freeze
+
   def index
     redirect_to root_path
   end
@@ -23,9 +26,21 @@ class TicketsController < ApplicationController
   end
 
   def edit
+    redirect_to root_path, flash: { error: TICKET_GOT_OFF } if @ticket.exited?
   end
 
   def update
+    if @ticket.exited?
+      redirect_to root_path, flash: { error: TICKET_GOT_OFF }
+      return
+    end
+
+    unless Gate.find(ticket_update_params[:exited_gate_id]).exit?(@ticket)
+      @ticket.errors[:base] << CAN_NOT_GET_OFF
+      render :edit
+      return
+    end
+
     if @ticket.update(ticket_update_params)
       redirect_to root_path, notice: '降車しました。😄'
     else
--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,17 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    section_number = calc_section_number(ticket)
+    return false unless section_number >= 1
+    ticket.fare >= calc_fare(section_number)
+  end
+
+  private
+  def calc_section_number(ticket)
+    (station_number - ticket.entered_gate.station_number).abs
+  end
+
+  def calc_fare(section_number)
+    FARES[section_number - 1]
   end
 end
