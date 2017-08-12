48e045181065c89ac364cf194cd4b221b587940a
toyokappa <kppg42@gmail.com>
2017-08-12 13:55:26 +0900

    Update gate model

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,33 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    if fare = calc_fare(ticket)
+      if fare <= ticket.fare
+        true
+      else
+        false
+      end
+    end
+  end
+
+  #チケットから駅番号を検索
+  def self.find_station_num(ticket)
+    find(ticket.entered_gate_id).station_number
+  end
+
+  #距離の計算
+  def calc_distance(ticket)
+    station_number - Gate.find_station_num(ticket)
+  end
+
+  #運賃を算出
+  def calc_fare(ticket)
+    if calc_distance(ticket) == 0
+      false
+    elsif calc_distance(ticket).abs == 1
+      Gate::FARES[0]
+    elsif calc_distance(ticket).abs == 2
+      Gate::FARES[1]
+    end
   end
 end
