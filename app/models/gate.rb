919cce93e759b343fdd5f6a414c5ba09a0ef1396
Yuji Takaesu <taka.esu.user@gmail.com>
2017-08-13 23:00:30 +0900

    Ex3. 同じ駅で降りる場合
    同じ駅判定をメソッドに切り出してstation_numberの比較に名前付け

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,7 +8,14 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    need_fare_idx = (station_number - ticket.entered_gate.station_number).abs - 1
+    entered_gate = ticket.entered_gate
+    return false if same_gate?(entered_gate)
+
+    need_fare_idx = (station_number - entered_gate.station_number).abs - 1
     FARES[need_fare_idx] <= ticket.fare
+  end
+
+  def same_gate?(entered_gate)
+    station_number == entered_gate.station_number
   end
 end
