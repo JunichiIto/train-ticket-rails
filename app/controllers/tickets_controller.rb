ea4a84be0b96ea049a5feb7662ea60704b14158b
totto357 <totto357@Naoto-no-MacBook-Air.local>
2017-08-14 15:29:02 +0900

    Ex4を回答

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -26,9 +26,11 @@ class TicketsController < ApplicationController
   end
 
   def update
-    if @ticket.update(ticket_update_params)
+    exited_gate = Gate.find(ticket_update_params[:exited_gate_id])
+    if exited_gate.exit?(@ticket) && @ticket.update(ticket_update_params)
       redirect_to root_path, notice: '降車しました。😄'
     else
+      flash.now[:error] = '降車駅 では降車できません。'
       render :edit
     end
   end
