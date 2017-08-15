b0192919e06e4d5bf3a229eda4c27800670188c3
totto357 <totto357@Naoto-no-MacBook-Air.local>
2017-08-15 10:58:59 +0900

    Gateモデルをリファクタリング

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -12,35 +12,32 @@ class Gate < ApplicationRecord
   # @params [Ticket] ticket 購入したチケット
   # @return [Boolean] true:出られる
   def exit?(ticket)
-    return false if ticket.nil?
-    return false if self == ticket.entered_gate
+    return false if self.same_station? ticket&.entered_gate
     calc_fare(ticket) <= ticket.fare
   end
 
-  # 他ゲートとの比較
-  # NOTE: クラスのチェックする？
-  #
-  # @params [Gate] other 比較対象のゲート
+  # 同一駅の改札かどうか
+  # @params [Gate] other_gate 比較対象の改札
   # @return [Boolean] 駅番号が一致する場合にtrue
-  def ==(other)
-    return false if other.nil?
-    self.station_number == other.station_number
+  def same_station?(other_gate)
+    return false unless other_gate.instance_of? Gate
+    self.station_number == other_gate.station_number
   end
 
   private
 
-    # チケットから運賃を計算する
-    # @params [Ticket] ticket 乗車チケット
-    # @return [Integer] 運賃
-    def calc_fare(ticket)
-      FARES[distance_from(ticket.entered_gate) - 1]
-    end
+  # チケットから運賃を計算する
+  # @params [Ticket] ticket 乗車チケット
+  # @return [Integer] 運賃
+  def calc_fare(ticket)
+    FARES[distance_from(ticket.entered_gate) - 1]
+  end
 
-    # 指定された改札がある駅との区間距離を取得する
-    # @params [Gate] gate 改札機
-    # @return [Integer] 改札との区間
-    def distance_from(gate)
-      (station_number - gate.station_number).abs
-    end
+  # 指定された改札がある駅との区間距離を取得する
+  # @params [Gate] gate 改札機
+  # @return [Integer] 改札との区間
+  def distance_from(gate)
+    (station_number - gate.station_number).abs
+  end
 
 end
