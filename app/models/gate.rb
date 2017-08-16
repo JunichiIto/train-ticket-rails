6b6ecf2d860078d2f989ee844973009e95bc3908
nishisuke <nishisuke13+git@gmail.com>
2017-08-16 17:31:01 +0900

    Pass all tests

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,6 +8,15 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    true
+    distance = self.relative_distance_between_another(ticket.entered_gate)
+    return false if distance.zero?
+    return ticket.fare >= FARES[distance-1]
+  rescue
+    false
+  end
+
+  protected
+  def relative_distance_between_another(gate)
+    (gate.station_number - self.station_number).abs
   end
 end
