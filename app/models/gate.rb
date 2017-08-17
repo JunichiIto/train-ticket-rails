c73310222894f1b844ad6f65c29dd2309a9b12c1
yosuke0315 <wonderfulworld0315@gmail.com>
2017-08-17 14:19:04 +0900

    When an inbound train.
    - Return `Gate#exit? #=> false` when fare is not enough.

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -8,7 +8,7 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
-    station_distance = station_number - ticket.entered_gate.station_number
+    station_distance = (station_number - ticket.entered_gate.station_number).abs
     return false if ticket.fare == 150 && station_distance == 2
     true
   end
