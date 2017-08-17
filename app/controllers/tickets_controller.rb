9bcd4e43a3323b3fd690e7536097b761e00c0569
yosuke0315 <wonderfulworld0315@gmail.com>
2017-08-17 14:37:12 +0900

    Redirect to edit when fare is not enough or same station.
    - Show error message `降車駅 では降車できません。😢`.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -26,11 +26,9 @@ class TicketsController < ApplicationController
   end
 
   def update
-    if @ticket.update(ticket_update_params)
-      redirect_to root_path, notice: '降車しました。😄'
-    else
-      render :edit
-    end
+    exited_gate = Gate.find(ticket_update_params[:exited_gate_id])
+    return redirect_to edit_ticket_path, alert: '降車駅 では降車できません。😢' unless exited_gate.exit?(@ticket)
+    return redirect_to root_path, notice: '降車しました。😄' if @ticket.update(ticket_update_params)
   end
 
   private
