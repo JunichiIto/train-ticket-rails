e00484dbaaa914882d4497b0d933b14b0c81aa35
yosuke0315 <wonderfulworld0315@gmail.com>
2017-08-17 15:08:58 +0900

    Redirect to root path when ticket was already used.
    - Show message `降車済みの切符です。😇`.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -23,10 +23,12 @@ class TicketsController < ApplicationController
   end
 
   def edit
+    return redirect_to root_path, alert: '降車済みの切符です。😇' if @ticket.already_used?
   end
 
   def update
     exited_gate = Gate.find(ticket_update_params[:exited_gate_id])
+    return redirect_to root_path, alert: '降車済みの切符です。😇' if @ticket.already_used?
     return redirect_to edit_ticket_path, alert: '降車駅 では降車できません。😢' unless exited_gate.exit?(@ticket)
     return redirect_to root_path, notice: '降車しました。😄' if @ticket.update(ticket_update_params)
   end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,9 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+
+  def already_used?
+    return true if entered_gate_id && exited_gate_id
+    false
+  end
 end
