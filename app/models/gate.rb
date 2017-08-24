fa34eec19d63f373bebce5fe12fa0fe823fc4d30
leon-joel <leopard10000@gmail.com>
2017-08-24 14:15:23 +0900

    Ex1. 運賃の計算（下り） Ex2. 運賃の計算（上り）

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -7,7 +7,15 @@ class Gate < ApplicationRecord
 
   scope :order_by_station_number, -> { order(:station_number) }
 
+  # 当駅で降りられる？＝運賃は足りている？
   def exit?(ticket)
-    true
+    # 乗車駅と降車駅（当駅）の区間数
+    traveled = (ticket.entered_gate.station_number - self.station_number).abs
+
+    # 区間数から所要運賃を算出 ※traveledが範囲外であることは想定しない
+    required_fare = FARES[traveled - 1]
+
+    # 切符の額面が所要運賃以上？
+    required_fare <= ticket.fare
   end
 end
